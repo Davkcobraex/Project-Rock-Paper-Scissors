3924d3cf5c7bc4c28d4866703cf98234b5c0692d
Davkcobraex <120555663+Davkcobraex@users.noreply.github.com>
2023-02-22 23:23:10 +1000

    Added playRound function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,27 @@
+const playsChoice = ["rock","paper","scissors"];
+
 function getComputerChoice(){
-    const playsChoice = ["Rock","Paper","Scissors"];
     let randInt = Math.floor(Math.random() * playsChoice.length)
     return playsChoice[randInt];
 }
 
 function playRound(playerSelection, computerSelection){
-
+    playerSelection = playerSelection.toLowerCase();
+    if (computerSelection == playerSelection){
+        return "Tie Game!"
+    }
+    else if (
+            (computerSelection == "paper" && playerSelection == "rock") ||
+            (computerSelection == "rock" && playerSelection == "scissors") ||
+            (computerSelection == "scissors" && playerSelection == "paper")
+            ){
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+    else {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
 }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
