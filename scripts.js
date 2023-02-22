1511990680876ec39b946cec83b636bcd51847fa
Davkcobraex <120555663+Davkcobraex@users.noreply.github.com>
2023-02-22 23:59:05 +1000

    Added Game Function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const playsChoice = ["rock","paper","scissors"];
+let wins = 0;
+let losts = 0;
 
 function getComputerChoice(){
     let randInt = Math.floor(Math.random() * playsChoice.length)
@@ -15,13 +17,30 @@ function playRound(playerSelection, computerSelection){
             (computerSelection == "rock" && playerSelection == "scissors") ||
             (computerSelection == "scissors" && playerSelection == "paper")
             ){
+        losts += 1;
         return `You Lose! ${computerSelection} beats ${playerSelection}`
     }
     else {
+        wins += 1;
         return `You Win! ${playerSelection} beats ${computerSelection}`
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    for (let i = 0; i < 5; i++) {
+        let sign = window.prompt("What is your play?");
+        console.log(playRound(sign, getComputerChoice()));
+    }
+    if (wins > losts){
+        console.log("you win!");
+    }
+    else {
+        console.log("you lost");
+    }
+}
+
+game();
+
+//const playerSelection = "rock";
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
